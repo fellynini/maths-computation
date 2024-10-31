3969c15f8a4123e6e16bb7b375abae4831bc51a9
fellynini <fellynini4@gmail.com>
2024-10-31 14:00:06 +0300

    three messages

---
+++ WelcomeToThreeMessages.java
@@ -0,0 +1,7 @@
+public class WelcomeToThreeMessages{
+public static void main(String[] args){
+System.out.println("i am Felly");
+System.out.println("i am new in java");
+System.out.println("java is fun");
+}
+}
---
+++ felly.java
@@ -0,0 +1,7 @@
+public class WelcomeToThreeMessages{
+public static void (String[] args){
+System.out.println("i am Felly");
+System.out.println("i am new in java");
+System.out.println("java is fun");
+}
+}
